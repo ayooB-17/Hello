4867e4b62e45dbc49afcfda64cb0e551a2b99513
Ayoob <ayoobpasqualli38@gmail.com>
2020-09-16 16:42:03 +0200

    my initial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello guys! How are you?")
+print("5+9")
+firstname=input("What is your name")
+print("My name is ", firstname)
+#======================AREA OF A CIRCLE=========================
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
